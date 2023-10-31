c671237f464cb4b66688e09c28c5bc508cd2042e
Andrew Glover <andrewcglover@gmail.com>
2023-10-31 17:57:13 +0000

    Appending access meanlife and receipt weights

--- scripts/utils/mdc.R
+++ scripts/utils/mdc.R
@@ -7,10 +7,17 @@ append_access_meanlife <- function(dataset) {
   dataset$prior_mean_access_meanlife <- rep(NA, dim(dataset)[1])
   for (i in 1:N_area_ids) {
     ids <- which(dataset$area_id == area_ids[i])
-    prior_mean_access_meanlife_i
+    dataset$prior_mean_access_meanlife[ids] <- prior_mean_access_meanlife[i]
   }
+  return(dataset)
 }
 
-grow_weights <- function(dataset) {
-  
+calculate_net_receipt_weights <- function(dataset) {
+  # hv005 = dhs weighting
+  # hml4 = months since net obtained
+  dhs_weight <- dataset$hv005
+  net_age <- dataset$hml4
+  growth_rate <- 1 / dataset$prior_mean_access_meanlife
+  dataset$receipt_weight <- dhs_weight * exp(net_age * growth_rate)
+  return(dataset)
 }
