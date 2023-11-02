1a54c69507bfae3f890f0c7f718cc41ea2cb54fe
Andrew Glover <andrewcglover@gmail.com>
2023-11-02 10:35:30 +0000

    added ksmth_fun code to estimate_MDC_timings

--- scripts/utils/mdc.R
+++ scripts/utils/mdc.R
@@ -144,11 +144,11 @@ combine_weights <- function(dataset, density_name) {
 #-------------------------------------------------------------------------------
 # Estimate MDC dates
 
-net_kernal_smooth <- function(den) {
-  
-}
-
 estimate_MDC_timings <- function(dataset, net_density_name = NULL) {
+  
+  mdc_nodes <- data.frame(area = character(),
+                          area_id = integer(),
+                          selected_mdc_nodes = integer())
   
   dataset$smth_nets <- rep(NA, dim(dataset)[1])
   
@@ -160,11 +160,95 @@ estimate_MDC_timings <- function(dataset, net_density_name = NULL) {
     kde_series <- ksmooth(CMC_series, area_net_density, 'normal',
                          bandwidth = ksmooth_bandwidth)
     dataset$smth_nets[area_ids] <- kde_series
+    
+    # Remaining code to the end of if N_modes > 0 statement copied from previous
+    # ksmth_fun on 02/11/23
+    kde_modes <- (kde_series > c(NA,kde_series[1:(length(kde_series)-1)])
+                  & kde_series > c(kde_series[2:length(kde_series)],NA))
+    max_kde_series <- max(kde_series)
+    camp_net_modes <- (kde_modes & kde_series > max_kde_series * prop_max_kde_mdc)
+    
+    camp_net_modes_id <- which(camp_net_modes)
+    camp_net_modes_kde <- kde_series[camp_net_modes_id]
+    camp_net_modes_kde_order <- order(camp_net_modes_kde, decreasing = TRUE)
+    N_modes <- length(camp_net_modes_kde)#min(c(length(camp_net_modes_kde), max_modes))
+    
+    selected_nodes <- rep(FALSE, N_CMC)
+    
+    if (N_modes > 0) {
+      
+      peak_checked_ordered_id <- c()
+      N_peaked <- 0
+      for (j in 1:N_modes) {
+        i <- camp_net_modes_kde_order[j]
+        lower_window <- max(1, camp_net_modes_id[i] - local_mode_window)
+        lower_avg_kde <- mean(kde_series[lower_window:(camp_net_modes_id[i]-1)])
+        upper_window <- min(N_CMC, camp_net_modes_id[i] + local_mode_window)
+        upper_avg_kde <- mean(kde_series[(camp_net_modes_id[i]+1):upper_window])
+        if ((kde_series[camp_net_modes_id[i]] > (lower_avg_kde*peak_window_ratio)
+             & kde_series[camp_net_modes_id[i]] > (upper_avg_kde*peak_window_ratio))) {
+          peak_checked_ordered_id <- c(peak_checked_ordered_id, camp_net_modes_id[i])
+          N_peaked <- N_peaked + 1
+        }
+      }
+      
+      
+      if (N_peaked > 0) {
+        selected_nodes_id <- peak_checked_ordered_id[1]
+        if (N_peaked > 1) {
+          for (j in 2:N_peaked) {
+            if (sum(abs(selected_nodes_id - peak_checked_ordered_id[j])
+                    < min_kde_int_mdc) == 0) {
+              selected_nodes_id <- c(selected_nodes_id, peak_checked_ordered_id[j])
+            } else {
+              node_included_after_adjustment <- FALSE
+              for (k in 1:length(selected_nodes_id)) {
+                if ( !node_included_after_adjustment &
+                     (abs(peak_checked_ordered_id[j] - selected_nodes_id[k]) < min_kde_int_mdc) ) {
+                  if (peak_checked_ordered_id[j] < selected_nodes_id[k]) {
+                    #check adjacent points to mode here - CHECK
+                    if ((CMC_series[peak_checked_ordered_id[j]] - min_kde_int_mdc) >= CMC_first) {
+                      peak_checked_ordered_id[j] <- selected_nodes_id[k] - min_kde_int_mdc
+                    }
+                  } else {
+                    if ((CMC_series[peak_checked_ordered_id[j]] + min_kde_int_mdc) <= CMC_last) {
+                      peak_checked_ordered_id[j] <- selected_nodes_id[k] + min_kde_int_mdc
+                    }
+                  }
+                  if (sum(abs(selected_nodes_id - peak_checked_ordered_id[j])
+                          < min_kde_int_mdc) == 0) {
+                    node_included_after_adjustment <- TRUE
+                  }
+                }
+              }
+              if (node_included_after_adjustment) {
+                selected_nodes_id <- c(selected_nodes_id, peak_checked_ordered_id[j])
+              }
+            }
+          }
+        }
+      }
+      
+      N_selected <- length(selected_nodes_id)
+      k <- N_peaked
+      while (N_selected > max_modes) {
+        selected_nodes_id <- selected_nodes_id[which(selected_nodes_id != peak_checked_ordered_id[k])]
+        N_selected <- N_selected - 1
+        k <- k - 1
+      }
+      
+      #selected_nodes <- rep(FALSE, N_CMC)
+      selected_nodes[selected_nodes_id] <- TRUE
+    }
+    
+    mdc_nodes <- data.frame(area = character(),
+                            area_id = integer(),
+                            selected_mdc_nodes = integer())
+    
+    mdc_nodes$area <- rep("area", length(selected_nodes))
+    mdc
+    
   }
-  
-  # Remaining code to the end of if N_modes > 0 statement copied from previous
-  # ksmth_fun on 02/11/23
-  
   
   return(dataset)
   
